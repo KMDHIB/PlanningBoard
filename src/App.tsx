ca2320b46d5e204e20b0889fce0148c28ac3af4a
Henrik Beske <hib@kmd.dk>
2024-11-08 11:21:10 +0100

    Ignore and stuff.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from './assets/uvdata.svg'
+import './App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,14 +8,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://uvdata.dk" target="_blank">
+          <img src={logo} className="logo react" alt="UVdata" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Skoleopskrivning</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
